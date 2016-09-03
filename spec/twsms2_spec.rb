60fc236c9158b0cc793f15936701ee90dfb9d354
Guanting Chen (@Idun) <cgt886@gmail.com>
2016-09-03 20:49:56 +0800

    update specs

--- spec/twsms2_spec.rb
+++ spec/twsms2_spec.rb
@@ -10,14 +10,14 @@ describe 'Twsms2::Client' do
     @sms_client = Twsms2::Client.new(username: @fake_username, password: @fake_password)
   end
 
-  describe 'when use send_message method with incorrect account' do
-    it 'must respond error' do
+  describe '以不存在的帳號密碼，使用 send_message 方法' do
+    it '必須回傳錯誤的結果，ERROR 的部分需要為 TWSMS:00010' do
       @sms_client.send_message.must_equal({:access_success=>false, :message_id=>nil, :error=>"TWSMS:00010"})
     end
   end
 
-  describe 'when use get_balance method with incorrect account' do
-    it 'must respond error' do
+  describe '以不存在的帳號密碼，使用 get_balance 方法' do
+    it '必須回傳錯誤的結果，ERROR 的部分需要為 TWSMS:00010' do
       @sms_client.get_balance.must_equal({:access_success=>false, :message_quota=>0, :error=>"TWSMS:00010"})
     end
   end
